af8d63ea2d2129ca86217d1d0e82a6016a668083
Wahhaj Farooq <wahhaj.farooq@gmail.com>
2022-05-09 01:07:07 -0500

    Update OCR.csproj and added unit tests

--- src/OCR.Tests/OCREngineTests.cs
+++ src/OCR.Tests/OCREngineTests.cs
@@ -1,24 +1,48 @@
+using System.IO;
+using System.Text.RegularExpressions;
 using NUnit.Framework;
 
 namespace OCR.Tests
 {
 	public class OCREngineTests
 	{
-		private IOCREngine ocrEnginerSUT;
+		private string _testTempDirectory;
+		private IOCREngine ocrEngineSUT;
 
 		[SetUp]
 		public void Setup()
 		{
-			ocrEnginerSUT = new OCREngine();
+			_testTempDirectory = TestHelper.GetTempDirectoryForTesting();
+			ocrEngineSUT = new OCREngine();
 		}
 
-		[Test]
-		public void TestGetTextFromImage()
+		[TearDown]
+		public void TearDown()
 		{
-			string extractedTextFromImage = ocrEnginerSUT.GetTextFromImage("");
+			if (Directory.Exists(_testTempDirectory))
+			{
+				Directory.Delete(_testTempDirectory, true);
+			}
+		}
+
+		[TestCase("CountOfMonteCristo.png", "CountOfMonteCristo.txt")]
+		[TestCase("PowerSupply.png", "PowerSupply.txt")]
+		[TestCase("TaleOfTwoCities.png", "TaleOfTwoCities.txt")]
+		public void TestGetTextFromImage(string inputImageFileName, string expectedTextFileName)
+		{
+			// Arrange
+			string testImageFilePath = Path.Combine(_testTempDirectory, inputImageFileName);
+			TestHelper.ExtractResourceFileNameIntoDestinationPathForTesting(inputImageFileName, testImageFilePath);
+
+			string testTextFilePath = Path.Combine(_testTempDirectory, expectedTextFileName);
+			TestHelper.ExtractResourceFileNameIntoDestinationPathForTesting(expectedTextFileName, testTextFilePath);
+
+			// Act
+			string extractedTextFromImage = ocrEngineSUT.GetTextFromImage(testImageFilePath);
 			
+			// Assert
 			Assert.That(extractedTextFromImage, Is.Not.Null.And.Not.Empty);
-			//Assert.That(extractedTextFromImage, Is.EqualTo(""));
+			Assert.That(Regex.Replace(extractedTextFromImage, "(?<!\r)\n", "\r\n"), Is.EqualTo(File.ReadAllText(testTextFilePath)));
 		}
 	}
 }
---
+++ src/OCR.Tests/TestHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace OCR.Tests;
+
+public static class TestHelper
+{
+	public static string GetTempDirectoryForTesting()
+	{
+		string fullTempDirectoryPath = Path.GetFullPath(Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString().Replace("-", "")));
+		Directory.CreateDirectory(fullTempDirectoryPath);
+		return fullTempDirectoryPath;
+	}
+
+	public static void ExtractResourceFileNameIntoDestinationPathForTesting(string resourceFileName, string resourceDestinationPath)
+	{
+		Assembly assembly = Assembly.GetExecutingAssembly();
+		string resourceFullName = assembly.GetManifestResourceNames().SingleOrDefault(name => name.EndsWith(resourceFileName));
+		using Stream resourceStream = assembly.GetManifestResourceStream(resourceFullName);
+		using FileStream resourceFileStream = new FileStream(resourceDestinationPath, FileMode.Create, FileAccess.Write);
+		resourceStream?.CopyTo(resourceFileStream);
+	}
+}
